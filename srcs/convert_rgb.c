5d2ddf18e85f21ca729ef51de83c5a2375cc2d16
PaceyChromeo <pcharlesjacob@gmail.com>
2021-11-24 21:57:26 +0100

    last push

--- srcs/convert_rgb.c
+++ srcs/convert_rgb.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   convert_rgb.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pjacob <pjacob@student.42.fr>              +#+  +:+       +#+        */
+/*   By: pacey <pacey@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/15 18:53:43 by hkrifa            #+#    #+#             */
-/*   Updated: 2021/11/24 14:18:39 by pjacob           ###   ########.fr       */
+/*   Updated: 2021/11/24 21:56:52 by pacey            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,19 +38,19 @@ int	init_texture(t_map *map)
 			&map->text[0].bpp, &map->text[0].line_length, &map->text[0].endian);
 	map->text[1].img_ptr = mlx_xpm_file_to_image(map->mlx_ptr,
 			map->south_text, &map->text[1].width, &map->text[1].height);
-	if (map->text[0].img_ptr == NULL)
+	if (map->text[1].img_ptr == NULL)
 		return (printf("Error : Cannot open SO"));
 	map->text[1].adr_text = (int *)mlx_get_data_addr(map->text[1].img_ptr,
 			&map->text[1].bpp, &map->text[1].line_length, &map->text[1].endian);
 	map->text[2].img_ptr = mlx_xpm_file_to_image(map->mlx_ptr, map->east_text,
 			&map->text[2].width, &map->text[2].height);
-	if (map->text[0].img_ptr == NULL)
+	if (map->text[2].img_ptr == NULL)
 		return (printf("Error : Cannot open EA"));
 	map->text[2].adr_text = (int *)mlx_get_data_addr(map->text[2].img_ptr,
 			&map->text[2].bpp, &map->text[2].line_length, &map->text[2].endian);
 	map->text[3].img_ptr = mlx_xpm_file_to_image(map->mlx_ptr, map->west_text,
 			&map->text[3].width, &map->text[3].height);
-	if (map->text[0].img_ptr == NULL)
+	if (map->text[3].img_ptr == NULL)
 		return (printf("Error : Cannot open WE"));
 	map->text[3].adr_text = (int *)mlx_get_data_addr(map->text[3].img_ptr,
 			&map->text[3].bpp, &map->text[3].line_length, &map->text[3].endian);
